034f112241e111183b71a38878628fbaf286bfcf
Emiton <emitonalves@gmail.com>
2018-05-17 11:34:25 -0400

    Added variables code along

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿// Section 3 Lecture 17 Demo 
+// Primitive Types: Variables and Constants
+
+using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Play with C# data types
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Emiton";
+            var isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            
+
+           // Learn how to input variables into a Console.WriteLine string
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            
+            // Use a constant
+            const float Pi = 3.14f;
+        }
+    }
+}
