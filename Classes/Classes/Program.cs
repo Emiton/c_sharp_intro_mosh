e52103bcaa9b92ef133d6d9387307db93ef15878
Emiton <emitonalves@gmail.com>
2018-05-16 22:08:45 -0400

    Added code along for class demo

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Classes.Math;
+using System;
+
+namespace Classes
+{ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            var calculator = new Calculator();
+            var result = calculator.Add(1,2);
+            Console.WriteLine(result);
+        }
+    }
+}
