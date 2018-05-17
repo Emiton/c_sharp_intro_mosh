039ab2693f21baa0ca71ba3d17ee02c74d8f5738
Emiton <emitonalves@gmail.com>
2018-05-17 11:37:06 -0400

    Added type conversion code along

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Perform explicit an explicit cast
+            int i = 1000;
+            byte b = (byte) i;
+            Console.WriteLine(b);
+            */
+
+            // Look at incompatible types
+
+            /*
+             * Create an exception when attempting to cast
+            string number = "1234";
+            int i = (int)number;
+            byte b = Convert.ToByte(number);
+            Console.WriteLine(b);
+            */
+
+            // Try your hand at handling exceptions
+
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+
+            }
+
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The string could not be converted to bool.");
+
+            }
+
+
+        }
+    }
+}
