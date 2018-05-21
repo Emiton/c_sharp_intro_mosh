2d17ff58c3012c4050735ef5aee68df436a3f887
Emiton <emiton_alves@my.uri.edu>
2018-05-21 17:57:11 -0400

    added demo using for each loops

---
+++ ForEachDemo/ForEachDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print every chracter in a string
+            var name = "Luke Skywalker";
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            // Simplify for loop by using foreach
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            // Same concept with int array
+            var numbers = new int[] { 1, 2, 3, 4 };
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
