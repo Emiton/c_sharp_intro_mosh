a1b91e55235b9cc4436d2c921e355dc4447e222e
Emiton <emiton_alves@my.uri.edu>
2018-06-01 15:19:06 -0400

    added working with files exercises

---
+++ WorkingWithFilesExercises/WorkingWithFilesExercises/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WorkingWithFilesExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\MEAN\TestFile.txt";
+            var wholeFile = File.ReadAllText(path);
+
+            // Write a program that opens a file and displays the number of words
+            Console.WriteLine(WordCounter(wholeFile));
+
+
+            // Write a program that reads a text file and displays the longest word
+            Console.WriteLine(FindLongestWord(wholeFile));
+        }
+
+
+
+        public static int WordCounter(string file)
+        {
+            var wordCount = 0;
+            var wordArray = file.Split(' ');
+            var punctuation = ",./\'\"\\:;`~+=-_!@#$%^&*(){}[]?";
+
+            foreach (var word in wordArray)
+            {
+                if (punctuation.IndexOf(word) == -1)
+                {
+                    wordCount++;
+                }
+            }
+
+            return wordCount;
+        }
+
+        public static string FindLongestWord(string file)
+        {
+            int maxLength = 0;
+            String longestWord = "";
+            var wordArray = file.Split(' ');
+
+            foreach (var word in wordArray)
+            {
+                if (word.Length > maxLength)
+                    longestWord = word;
+            }
+
+            return longestWord;
+        }
+
+    }
+}
